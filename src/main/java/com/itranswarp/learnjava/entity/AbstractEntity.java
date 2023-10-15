8cef90ac1391bc081837c86d3ba0e5fdd089bafa
huang104160 <76827879+huang104160@users.noreply.github.com>
2023-10-15 20:52:04 +0800

    Initial commit

---
+++ src/main/java/com/itranswarp/learnjava/entity/AbstractEntity.java
@@ -0,0 +1,50 @@
+package com.itranswarp.learnjava.entity;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Transient;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    private Long id;
+    private long createdAt;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false, updatable = false)
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Transient
+    public ZonedDateTime getCreatedDateTime() {
+        return Instant.ofEpochMilli(this.createdAt).atZone(ZoneId.systemDefault());
+    }
+
+    @PrePersist
+    public void preInsert() {
+        setCreatedAt(System.currentTimeMillis());
+    }
+}
